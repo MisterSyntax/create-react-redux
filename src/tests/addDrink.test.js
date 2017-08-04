71ced51733b046e3f4a1a8f7e7e52596335c79c0
MisterSyntax <asyriala@gmail.com>
2017-08-04 16:29:28 -0400

    removed redundancies

--- src/tests/addDrink.test.js
+++ src/tests/addDrink.test.js
@@ -1,27 +1,59 @@
 import state from '../initialState.json'
 import { addDrink, removeDrink } from '../actions'
 
+import C from '../constants'
+import { drinks } from '../store/reducers'
+import { createStore } from 'redux'
+import appReducer from '../store/reducers';
+
+
+
+describe('ADD_DRINK', function () {
+    const store = createStore(appReducer, state);
+    const action = {
+        type: C.ADD_DRINK,
+        payload: {
+            "name": "Budweiser",
+            "bar": "Last Drop",
+            "geo": "TODO-12",
+            "address": "",
+            "price": 3,
+            "size": 16,
+            "totalDrinks": 1,
+            "lastDrank": "2017-06-23",
+            "drinkId": 10,
+            "flags": {
+                "badPrice": 0,
+                "unavailable": 0
+            }
+        }
+    }
+    const nextState = drinks(state.drinks, action);
+    test('increase amounts of drinks by 1', () => {
+        expect(nextState.length).toBe(3)
+    });
+});
+
+
 //store for tests
 
-import appReducer from '../store/reducers';
-import { createStore } from 'redux';
 
-const store = createStore(appReducer, state);
+describe("ADD Drink action", function () {
 
-describe("ADD Drink", function () {
+    const store = createStore(appReducer, state);
     const drink = {
-            name: "Buff's Red",
-            bar: "Buff's Pub",
-            price: 4.5,
-            size: 16,
-            drinkId: 10,
-            geo: {
-                lat: 42.3571451,
-                lng: -71.18658920000001
-            },
-            address: "317 Washington St, Newton, MA 02458, USA"
-        }
-        store.dispatch(addDrink(drink));
+        name: "Buff's Red",
+        bar: "Buff's Pub",
+        price: 4.5,
+        size: 16,
+        drinkId: 10,
+        geo: {
+            lat: 42.3571451,
+            lng: -71.18658920000001
+        },
+        address: "317 Washington St, Newton, MA 02458, USA"
+    }
+    store.dispatch(addDrink(drink));
 
     test('increase amounts of drinks by 1', () => {
         expect(store.getState().drinks.length).toBe(3)
