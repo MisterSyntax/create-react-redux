2829bdb08f3179339c963a10a7edcda891150bfa
MisterSyntax <asyriala@gmail.com>
2017-08-04 15:35:25 -0400

    added tests for ADD/REMOVE drink

--- src/test/reducers.test.js
+++ src/test/reducers.test.js
@@ -1,17 +1,20 @@
-import C from '../constants';
-import state from '../src/initialState.json';
+import C from '../constants'
+import state from '../src/initialState.json'
 
+import { drinks } from '../store/reducers'
+import { createStore } from 'redux'
+import appReducer from '../src/store/reducers';
 
 describe('ADD_DRINK', function () {
   let nextState = {};
   test('increase amounts of drinks by 1', () => {
-    expect(1).toBe(21);
+    expect(1).toBe(21)
   });
 });
 
 describe('REMOVE_DRINK', function () {
   let nextState = {};
   test('increase amounts of drinks by 1', () => {
-    expect(1).toBe(21);
+    expect(1).toBe(21)
   });
 });
