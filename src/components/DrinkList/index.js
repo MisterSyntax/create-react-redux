ab9645049cc285c37b678eebfbfe2e7fa49939f1
MisterSyntax <asyriala@gmail.com>
2017-08-04 16:02:16 -0400

    added DrinkList test

---
+++ src/components/DrinkList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class DrinkList extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+        <div id="drink-list">
+
+        </div>)
+    }
+}
